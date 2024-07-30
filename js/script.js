773af8666327621d7a00aa963f266437a8b5d193
rodlez <xavier.rodlez@gmail.com>
2024-07-30 13:53:25 +0200

    Add JS to the top button
    When clicked the page scroll all the way to the top

--- js/script.js
+++ js/script.js
@@ -4,14 +4,18 @@
  * */
 function userScroll(startPixel, colorChange) {
     const navbar = document.querySelector('.navbar');
+    // To Show or Hide the top button when we scroll
+    const toTopBtn = document.querySelector('#to-top');
 
     window.addEventListener('scroll', () => {
         if(window.scrollY > startPixel) {
             navbar.classList.add(colorChange);
             navbar.classList.add('navbar-sticky');
+            toTopBtn.classList.add('show');
         } else {
             navbar.classList.remove(colorChange);
             navbar.classList.remove('navbar-sticky');
+            toTopBtn.classList.remove('show');
         }
     });
 }
@@ -41,9 +45,19 @@ function incrementStats(delay) {
   
       updateCounter();
     });
-  }
+}
 
+
+/**
+ * Position the DOM on the top
+ */
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 // Once the DOM id loaded the functions will be executed
 document.addEventListener('DOMContentLoaded', userScroll(50, 'bg-white'));
 document.addEventListener('DOMContentLoaded', incrementStats(10));
+// Event Listener when top button is clicked
+document.querySelector('#to-top').addEventListener('click', scrollToTop);
